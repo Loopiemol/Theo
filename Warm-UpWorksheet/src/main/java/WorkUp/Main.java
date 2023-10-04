3cc072ec6c0b7c7f9c8ba56a378de629d511d717
Theo Adodoadji <theophilinea@gmail.com>
2023-10-04 10:25:44 -0500

    theo

---
+++ Warm-UpWorksheet/src/main/java/WorkUp/Main.java
@@ -0,0 +1,15 @@
+package WarmUp;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println();
+
+        String personName = "JohnDoe";
+        int personAge = 25;
+        double productPrice = 19.99;
+        float temperatureCelsius = 27.5f;
+        Boolean isLoggedIn = true;
+    }
+}
