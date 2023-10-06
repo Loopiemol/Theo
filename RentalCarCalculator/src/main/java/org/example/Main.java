a4f091c2c67bd29ed03f817eb19b73aee69bc910
Theo Adodoadji <theophilinea@gmail.com>
2023-10-05 21:36:52 -0500

    theo

--- RentalCarCalculator/src/main/java/org/example/Main.java
+++ RentalCarCalculator/src/main/java/org/example/Main.java
@@ -7,71 +7,64 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("What is your current age? Please enter a whole number?");
-        int numberOfDays = scanner.nextInt();
+        int currentAge = scanner.nextInt();
         scanner.nextLine();
 
         System.out.println("What is the pick-up date? Type like this: Month/Day/Year");
         String pickUpDate = scanner.nextLine();
 
         System.out.println("How many days will you be keeping the car for?");
-        double number0fDays = scanner.nextDouble();
+        double numberOfDays = scanner.nextDouble();
         scanner.nextLine();
 
-        double ageFee = numberOfDays * .30;
-        if (numberOfDays < 25) {
-            System.out.println("You have to pay a fee. Sorry.");
-        } else {
-            System.out.println("You don't have to pay a fee!");
-
+        double ageFee = 0;
+        if (currentAge < 25) {
+            ageFee = numberOfDays * 0.30 * 29.99;
+        }
+        else {
+            ageFee = 0;
         }
 
         System.out.println("Do you want an electronic toll tag at $3.95 a day?");
-        String tollTag = scanner.nextLine();
-        tollTag = tollTag.toLowerCase();
-        double tollTagfee = 0;
+        String tollTag = scanner.nextLine().toLowerCase();
+        double tollTagFee = 0;
         switch (tollTag) {
             case "yes":
-                tollTagfee = numberOfDays * 3.95;
+                tollTagFee = numberOfDays * 3.95;
                 break;
             case "no":
+                tollTagFee = 0;
                 break;
-
 
         }
 
         System.out.println("Do you want a GPS at $2.95 a day?");
-        String gps = scanner.nextLine();
-        gps = gps.toLowerCase();
+        String gps = scanner.nextLine().toLowerCase();
+        double gpsFee = 0;
         switch (gps) {
             case "yes":
-                double gpsFee = numberOfDays * 2.95;
+                gpsFee = numberOfDays * 2.95;
                 break;
             case "no":
+                gpsFee = 0;
                 break;
-
 
         }
 
         System.out.println("Do you want roadside assistance at $3.95 a day?");
-        String roadSideAssistance = scanner.nextLine();
-        roadSideAssistance = roadSideAssistance.toLowerCase();
+        String roadSideAssistance = scanner.nextLine().toLowerCase();
         double roadSideAssistanceFee = 0;
         switch (roadSideAssistance) {
             case "yes":
                 roadSideAssistanceFee = numberOfDays * 3.95;
                 break;
             case "no":
+                roadSideAssistanceFee = 0;
                 break;
-
-
         }
 
         double basePrice = 29.99 * numberOfDays;
-        double tollTagfeezero = 0;
-        double gpsFeezeroe = 0;
-        double roadSideAssistanceFeezero = 0;
-
-        double totalCost = basePrice + ageFee + tollTagfee + tollTagfeezero + gpsFeezeroe + roadSideAssistanceFee + roadSideAssistanceFeezero;
+        double totalCost = basePrice + ageFee + tollTagFee + gpsFee + roadSideAssistanceFee;
         System.out.printf("Your pick-up date is %s. Your total is $%.2f dollars%n", pickUpDate, totalCost);
     }
 }
